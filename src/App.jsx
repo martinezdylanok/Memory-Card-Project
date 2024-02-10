c9fbfd0a344a886fd4c201541a4c6beb12402c52
Lpik97 <dm97fx@gmail.com>
2024-02-10 11:05:14 +0100

    feat(App): Integrate Game component
    Update the App component to include the Game component, allowing it to be rendered alongside GameInfo and GameScore.

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,14 @@
 import "./App.css";
 import GameInfo from "./components/GameInfo";
 import GameScore from "./components/GameScore";
+import Game from "./components/Game";
 
 export default function App() {
    return (
       <>
          <GameInfo />
          <GameScore />
+         <Game />
       </>
    );
 }
