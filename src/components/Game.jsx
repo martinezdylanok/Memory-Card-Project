f5815d994a387590422cf61d08605363fa47f248
Lpik97 <dm97fx@gmail.com>
2024-02-10 11:03:34 +0100

    feat(Game): Add Game component
    Introduce a new React component named Game, responsible for rendering a grid of cards for the game.

---
+++ src/components/Game.jsx
@@ -0,0 +1,33 @@
+export default function Game() {
+   return (
+      <div>
+         <div className="card">
+            <img src="" alt="" />
+         </div>
+         <div className="card">
+            <img src="" alt="" />
+         </div>
+         <div className="card">
+            <img src="" alt="" />
+         </div>
+         <div className="card">
+            <img src="" alt="" />
+         </div>
+         <div className="card">
+            <img src="" alt="" />
+         </div>
+         <div className="card">
+            <img src="" alt="" />
+         </div>
+         <div className="card">
+            <img src="" alt="" />
+         </div>
+         <div className="card">
+            <img src="" alt="" />
+         </div>
+         <div className="card">
+            <img src="" alt="" />
+         </div>
+      </div>
+   );
+}
