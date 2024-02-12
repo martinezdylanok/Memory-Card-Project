009e70780ccb8786f0b3b99475e41f10991a256a
Lpik97 <dm97fx@gmail.com>
2024-02-12 08:01:00 +0100

    refactor(GameInfo): Update game instructions
    Update the GameInfo component to provide clearer instructions for playing the Memory Card Game.

--- src/components/GameInfo.jsx
+++ src/components/GameInfo.jsx
@@ -2,7 +2,7 @@ export default function GameInfo() {
    return (
       <>
          <h1>Memory Card Game</h1>
-         <span>Get points by clicking on two same images</span>
+         <span>Get points by clicking on two same gifs</span>
       </>
    );
 }
