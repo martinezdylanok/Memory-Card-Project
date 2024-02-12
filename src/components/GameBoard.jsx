d73bd2d43498f9f5ca97874d94b14923378b2ab7
Lpik97 <dm97fx@gmail.com>
2024-02-12 08:02:50 +0100

    feat(GameBoard): Add GameBoard component
    Introduce a new React component named GameBoard, responsible for rendering the game board with GIFs and displaying the game score.

---
+++ src/components/GameBoard.jsx
@@ -0,0 +1,27 @@
+import React from "react";
+import GameScore from "./GameScore";
+import Gif from "./Gif";
+
+export default function GameBoard() {
+   return (
+      <div className="game-board">
+         <GameScore />
+         <Gif searchTerm="elefante" />
+         <Gif searchTerm="yellow" />
+         <Gif searchTerm="red" />
+         <Gif searchTerm="green" />
+         <Gif searchTerm="elefante" />
+         <Gif searchTerm="elefante" />
+         <Gif searchTerm="elefante" />
+         <Gif searchTerm="elefante" />
+         <Gif searchTerm="elefante" />
+         <Gif searchTerm="elefante" />
+         <Gif searchTerm="elefante" />
+         <Gif searchTerm="elefante" />
+         <Gif searchTerm="elefante" />
+         <Gif searchTerm="elefante" />
+         <Gif searchTerm="elefante" />
+         <Gif searchTerm="elefante" />
+      </div>
+   );
+}
