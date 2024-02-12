c95e0483f78d106f6a5ece662d2cd8c594635301
Lpik97 <dm97fx@gmail.com>
2024-02-12 07:56:01 +0100

    refactor(App): Update component structure
    Modify the App component to replace GameScore with GameBoard, reflecting changes in component structure.

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,12 @@
 import "./App.css";
 import GameInfo from "./components/GameInfo";
-import GameScore from "./components/GameScore";
-import Game from "./components/Game";
+import GameBoard from "./components/GameBoard";
 
 export default function App() {
    return (
       <>
          <GameInfo />
-         <GameScore />
-         <Game />
+         <GameBoard />
       </>
    );
 }
