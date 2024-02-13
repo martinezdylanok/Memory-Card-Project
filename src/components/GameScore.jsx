c56e8f5bae85fa9e6b121e751e72d29f3fdc16dc
Lpik97 <dm97fx@gmail.com>
2024-02-13 15:57:48 +0100

    refactor(GameScore): Update GameScore component
    Update the GameScore component to wrap the score display elements in a container with appropriate styling.

--- src/components/GameScore.jsx
+++ src/components/GameScore.jsx
@@ -1,8 +1,11 @@
+import React from "react";
+import "../styles/GameScore.css";
+
 export default function GameScore() {
    return (
-      <>
+      <div className="score-container">
          <span>Score:</span>
          <span>Best Score:</span>
-      </>
+      </div>
    );
 }
