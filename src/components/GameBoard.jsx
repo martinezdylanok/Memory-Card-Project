10d57f3a58c67d2e0e4037b579d6be39438fcad8
Lpik97 <dm97fx@gmail.com>
2024-02-13 15:55:08 +0100

    feat(GameBoard): Update GameBoard component
    Modify the GameBoard component to include shuffled GIFs fetched from the Giphy API and display them within a container.

--- src/components/GameBoard.jsx
+++ src/components/GameBoard.jsx
@@ -1,27 +1,24 @@
 import React from "react";
 import GameScore from "./GameScore";
 import Gif from "./Gif";
+import "../styles/GameBoard.css";
+import concatenateArray from "../modules/concatenateArray";
+import shuffleArray from "../modules/shuffleArray";
 
 export default function GameBoard() {
+   const randomGifsArray = ["elephant", "red", "computer", "boat", "mouse", "phone", "jacket", "singer"];
+   const randomGifsCopy = randomGifsArray;
+   const concatenatedArray = concatenateArray(randomGifsArray, randomGifsCopy);
+   const shuffledArray = shuffleArray(concatenatedArray);
+
    return (
       <div className="game-board">
          <GameScore />
-         <Gif searchTerm="elefante" />
-         <Gif searchTerm="yellow" />
-         <Gif searchTerm="red" />
-         <Gif searchTerm="green" />
-         <Gif searchTerm="elefante" />
-         <Gif searchTerm="elefante" />
-         <Gif searchTerm="elefante" />
-         <Gif searchTerm="elefante" />
-         <Gif searchTerm="elefante" />
-         <Gif searchTerm="elefante" />
-         <Gif searchTerm="elefante" />
-         <Gif searchTerm="elefante" />
-         <Gif searchTerm="elefante" />
-         <Gif searchTerm="elefante" />
-         <Gif searchTerm="elefante" />
-         <Gif searchTerm="elefante" />
+         <div className="gif-container">
+            {shuffledArray.map((gif, index) => (
+               <Gif key={`${gif}-${index}`} searchTerm={gif} />
+            ))}
+         </div>
       </div>
    );
 }
