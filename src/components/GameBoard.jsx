b9f4832387d82245b8e6fd9fc60f465a04fca5c1
Lpik97 <dm97fx@gmail.com>
2024-02-16 18:24:24 +0100

    refactor(GameBoard): Update GameBoard component logic
    Update the GameBoard component to fetch GIF data for multiple words, interleave them, shuffle the array, and handle GIF click events for game interaction.

--- src/components/GameBoard.jsx
+++ src/components/GameBoard.jsx
@@ -1,24 +1,54 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import GameScore from "./GameScore";
 import Gif from "./Gif";
 import "../styles/GameBoard.css";
-import concatenateArray from "../modules/concatenateArray";
+import generateArray from "../modules/generateArray";
+import fetchGif from "../modules/fetchGif";
 import shuffleArray from "../modules/shuffleArray";
 
 export default function GameBoard() {
-   const randomGifsArray = ["elephant", "red", "computer", "boat", "mouse", "phone", "jacket", "singer"];
-   const randomGifsCopy = randomGifsArray;
-   const concatenatedArray = concatenateArray(randomGifsArray, randomGifsCopy);
-   const shuffledArray = shuffleArray(concatenatedArray);
+   const [gifType, setGifType] = useState([]);
+   const [activeGifs, setActiveGifs] = useState([]);
+
+   useEffect(() => {
+      const wordsArray = generateArray();
+
+      Promise.all(wordsArray.map((word) => fetchGif(word)))
+         .then((allGifsData) => {
+            const gifsData = allGifsData.flat();
+            const interleavedGifsData = [];
+            gifsData.forEach((gif) => {
+               interleavedGifsData.push(gif);
+               interleavedGifsData.push({ ...gif, id: `${gif.id}_copy` });
+            });
+            const shuffledArray = shuffleArray(interleavedGifsData);
+            setGifType(shuffledArray);
+         })
+         .catch((error) => {
+            console.error(error);
+         });
+   }, []);
+
+   const handleGifClick = (gifId) => {
+      if (!activeGifs.includes(gifId)) {
+         if (activeGifs.length < 2) {
+            setActiveGifs([...activeGifs, gifId]);
+         } else {
+            setActiveGifs([]);
+         }
+      }
+   };
 
    return (
       <div className="game-board">
          <GameScore />
-         <div className="gif-container">
-            {shuffledArray.map((gif, index) => (
-               <Gif key={`${gif}-${index}`} searchTerm={gif} />
+         <ul className="gif-container">
+            {gifType.map((gif) => (
+               <li key={gif.id}>
+                  <Gif url={gif.images.original.url} isVisible={activeGifs.includes(gif.id)} changeVisibility={() => handleGifClick(gif.id)} />
+               </li>
             ))}
-         </div>
+         </ul>
       </div>
    );
 }
